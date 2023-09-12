eb78b97fc402f3da113a5c3247e9f63d08798588
Mia077 <144052943+Mia077@users.noreply.github.com>
2023-09-12 17:46:57 +0300

    The related Changes did for the first time!!!

--- src/main/java/teckwil/com/Main.java
+++ src/main/java/teckwil/com/Main.java
@@ -1,5 +1,14 @@
-package teckwil.com;public class Main {
+package teckwil.com;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int nrTren;
+        char nume;
+        String garaDePlecare;
+
+        nume = 'B';
+        nrTren = 323;
+        garaDePlecare = 'New';
     }
 }
