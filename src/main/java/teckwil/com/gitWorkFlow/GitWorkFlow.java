1f434cbd829e1978e52d91e709f0e7aaf73b80db
Mia077 <144052943+Mia077@users.noreply.github.com>
2023-09-13 20:09:51 +0300

    The related Changes did for git modification time!!!

---
+++ src/main/java/teckwil/com/gitWorkFlow/GitWorkFlow.java
@@ -0,0 +1,12 @@
+package teckwil.com.gitWorkFlow;
+
+public class GitWorkFlow {
+    public static void main(String[] args){
+        String step1 = "Execute git stash in the console";
+        String step2 = "Perform git checkout master - to move to the local master branch";
+        String step3 = "Execute git fetch command - to see if there are any changes on the origin/master";
+        String step4 = "Execute git pull - this will downland the latest changes from origin?master to local master";
+        String step5 = "Execute git branch New_Branch_Name command to create a new  branch that is same as local master";
+        String step6 = "Perform git checkout New_Branch_Name ";
+    }
+}
