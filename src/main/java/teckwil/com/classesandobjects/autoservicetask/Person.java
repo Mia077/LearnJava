5daf2c4ae386019d5c9bfe07d72affacb3a7cd32
Mia077 <144052943+Mia077@users.noreply.github.com>
2023-09-19 17:19:12 +0300

    Propreties off classess , objects,and constructors. Autoservice tasck lesson8.

---
+++ src/main/java/teckwil/com/classesandobjects/autoservicetask/Person.java
@@ -0,0 +1,9 @@
+package teckwil.com.classesandobjects.autoservicetask;
+
+public class Person  {
+    public String phoneNumber;
+
+    public Person(String enteredThePhoneNumberAtInitializationStage){
+        phoneNumber = enteredThePhoneNumberAtInitializationStage;
+    }
+}
---
+++ src/main/java/teckwil/com/classesandobjects/autoservicetask/Worker.java
@@ -0,0 +1,10 @@
+package teckwil.com.classesandobjects.autoservicetask;
+
+public class Worker {
+    public String name;
+    public int age;
+
+    public Worker(){
+        System.out.println(" Un Obiect de tip worker este creat, acest constructor este fara parametri.  ");
+    }
+}
