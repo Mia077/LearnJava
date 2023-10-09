4ca380cce90c7caa4ad6e9162a7ef3147cde183c
Mia077 <144052943+Mia077@users.noreply.github.com>
2023-10-09 09:32:09 +0300

    Added the implementation of file management service that will be used for the ATF.

---
+++ src/main/java/teckwil/com/filemanagementservice/OutputFileWriterManager.java
@@ -0,0 +1,41 @@
+package teckwil.com.filemanagementservice;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class OutputFileWriterManager {
+    public static void writeContentIntoFileUsingWriter(String filePath, String content){
+        FileWriter fileWriter = null;
+        try{
+            fileWriter = new FileWriter(filePath);
+            fileWriter.write(content);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (fileWriter != null)
+                fileWriter.close();
+            }catch (IOException  e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void writeContentIntoFileUsingBufferWriter(String filePath, String content){
+        BufferedWriter bufferedWriter = null;
+
+        try {
+            bufferedWriter = new BufferedWriter(new FileWriter(filePath));
+            bufferedWriter.write(content);
+        } catch (IOException e ) {
+            e.printStackTrace();
+        }finally {
+            try {
+            if (bufferedWriter != null)
+                bufferedWriter.close();
+        } catch (IOException e ) {
+            e.printStackTrace();
+            }
+        }
+    }
+}
